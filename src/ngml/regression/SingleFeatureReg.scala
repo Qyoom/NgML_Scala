c9b823b14816db4dfcdc487369241e546169fcb4
GitOutATown <richard@aqume.com>
2014-05-25 18:32:06 -0700

    Starting second try at gradientDescent implementation

--- src/ngml/regression/SingleFeatureReg.scala
+++ src/ngml/regression/SingleFeatureReg.scala
@@ -18,8 +18,8 @@ object SingleFeatureReg {
         
         // Bias prepended to independent variable (city population)
         val X:List[List[Double]] = data map(_ match {
-            case x :: xs => 1.0 :: List[Double](x)
-            case _ => throw new NoSuchElementException
+			case x :: xs => 1.0 :: List[Double](x)
+			case _ => throw new NoSuchElementException
         })
         println("X: " + X)
         
@@ -33,21 +33,63 @@ object SingleFeatureReg {
         
         val theta = List(List(0.0), List(0.0))
         
-        val iterations = 1500 // regression cycles
-        val alpha = 1.01 // gradient step size
+        //val iterations = 1500 // regression cycles
+        val iterations = 15 // diagnostic
+        val alpha = 0.01 // gradient step size
         
+        // Initial cost calc
         val J = computeCost(X, y, theta)
         println("J: " + J)
+        
+        // theta calc via gradient descent
+        val result:((Double, Double), List[Double]) = gradientDescent(X, y, theta, alpha, iterations)
+        println("main - result: " + result)
     }
     
     def computeCost(X: List[List[Double]], y: List[Double], theta: List[List[Double]]): Double = {
         val m = y.length
         val predictions = matxProd(X, theta).flatten
         val sqrErrors = dotPow(diff(predictions, y), 2)
-        val sqrErrTot = sqrErrors.reduceLeft(_ + _)
+        val sqrErrTotal = sqrErrors.reduceLeft(_ + _)
         val mu = (1.0/(2*m))
-        val J = mu * sqrErrTot
+        val J = mu * sqrErrTotal
         J // cost
     }
+    
+    def gradientDescent(
+        X: List[List[Double]],
+        y: List[Double], 
+        theta: List[List[Double]],
+        alpha: Double, 
+        iterations: Int
+		): ((Double, Double), List[Double]) = {
+        
+        val m = y.length
+        val x = X map (x => x(1))
+                
+        def iterGradDesc(iter: Int) {
+            if(!(iter > iterations)) { // recursion/iteration bounds
+                
+                val thetaTemp = (0.0, 0.0)
+                
+                val errorsSum = diff(matxProd(X, theta).flatten, y).reduceLeft(_+_)
+                
+                iterGradDesc(iter + 1) // recursive iteration
+            }
+        }
+        
+        iterGradDesc(0)
+        
+        ((0.0, 0.0), List(0.0)) // STUB - NIX
+    }
 
+    def diagnostic(elem: Any) {
+        println(elem)
+    }
 }
+
+
+
+
+
+
