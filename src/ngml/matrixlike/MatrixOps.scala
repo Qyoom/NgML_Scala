918a92c60853505cad99f2166da2a51e00d5372c
Richard Walker <richard@aqume.com>
2014-05-26 19:25:29 -0700

    Update MatrixOps.scala
    fixing github indentation glitches

--- src/ngml/matrixlike/MatrixOps.scala
+++ src/ngml/matrixlike/MatrixOps.scala
@@ -13,7 +13,7 @@ object MatrixOps {
 
     // http://en.wikipedia.org/wiki/Dot_product
     def dotProd(v1: List[Double], v2: List[Double]): Double = {
-		require(v1.length == v2.length, "dotProduct - Rows must be of equal length. " + 
+	require(v1.length == v2.length, "dotProduct - Rows must be of equal length. " + 
             "v1.length;" + v1.length + " v2.length:" + v2.length +
             "\nv1: " + v1 + " v2: " + v2)
         val zipped = v1 zip v2
@@ -22,21 +22,21 @@ object MatrixOps {
     }
   
     def transpose(m: List[List[Double]]): List[List[Double]] = { // Recursive
-	    if(m.head.isEmpty) Nil // convergence condition
-	    else {
-		    val t_head: List[Double] = m.map(_ head) // forms a column made from the head of each row
-		    val m_tails = m.map(_ tail) // forms a new matrix minus the former first column
-		    val recurseTranspOfTails: List[List[Double]] = transpose(m_tails)
-		    t_head :: recurseTranspOfTails // cons each new transposed "column" onto a new Matrix
-	    }
+    	if(m.head.isEmpty) Nil // convergence condition
+    	else {
+	    val t_head: List[Double] = m.map(_ head) // forms a column made from the head of each row
+	    val m_tails = m.map(_ tail) // forms a new matrix minus the former first column
+	    val recurseTranspOfTails: List[List[Double]] = transpose(m_tails)
+	    t_head :: recurseTranspOfTails // cons each new transposed "column" onto a new Matrix
 	}
-    
+    }
+
     def diff(v1: List[Double], v2: List[Double]): List[Double] = {
-	  	require(v1.length == v2.length, "diff - inputs must be of equal length.")
-	  	val zipped = v1 zip v2
-	  	val difRes = zipped map { tup: (Double, Double) => tup._1 - tup._2 }
-	  	difRes
-	}
+  	require(v1.length == v2.length, "diff - inputs must be of equal length.")
+  	val zipped = v1 zip v2
+  	val difRes = zipped map { tup: (Double, Double) => tup._1 - tup._2 }
+  	difRes
+    }
     
     def dotPow(v: List[Double], exp: Double): List[Double] = {
         v map (n => pow(n, exp))
