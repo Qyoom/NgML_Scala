926edfad2fe369ff2f21c0dc21505ec98f82a8fe
GitOutATown <richard@aqume.com>
2014-05-27 11:06:50 -0700

    Edited comments

--- src/ngml/matrixlike/MatrixOps.scala
+++ src/ngml/matrixlike/MatrixOps.scala
@@ -6,14 +6,14 @@ object MatrixOps {
     
     // Matrix multiplication
     def matxProd(m1: List[List[Double]], m2: List[List[Double]]): List[List[Double]] = {
-	for(m1row <- m1) yield
-	for(m2col <- transpose(m2)) yield
-	dotProd(m1row, m2col)
+		for(m1row <- m1) yield
+		for(m2col <- transpose(m2)) yield
+		dotProd(m1row, m2col)
     }
 
     // http://en.wikipedia.org/wiki/Dot_product
     def dotProd(v1: List[Double], v2: List[Double]): Double = {
-	require(v1.length == v2.length, "dotProduct - Rows must be of equal length. " + 
+		require(v1.length == v2.length, "dotProduct - Rows must be of equal length. " + 
             "v1.length;" + v1.length + " v2.length:" + v2.length +
             "\nv1: " + v1 + " v2: " + v2)
         val zipped = v1 zip v2
@@ -24,21 +24,27 @@ object MatrixOps {
     def transpose(m: List[List[Double]]): List[List[Double]] = { // Recursive
     	if(m.head.isEmpty) Nil // convergence condition
     	else {
-	    val t_head: List[Double] = m.map(_ head) // forms a column made from the head of each row
-	    val m_tails = m.map(_ tail) // forms a new matrix minus the former first column
-	    val recurseTranspOfTails: List[List[Double]] = transpose(m_tails)
-	    t_head :: recurseTranspOfTails // cons each new transposed "column" onto a new Matrix
-	}
+		    val t_head: List[Double] = m.map(_ head) // forms a column made from the head of each row
+		    val m_tails = m.map(_ tail) // forms a new matrix minus the former first column
+		    val recurseTranspOfTails: List[List[Double]] = transpose(m_tails)
+		    t_head :: recurseTranspOfTails // cons each new transposed "column" onto a new Matrix
+		}
     }
 
     def diff(v1: List[Double], v2: List[Double]): List[Double] = {
-  	require(v1.length == v2.length, "diff - inputs must be of equal length.")
-  	val zipped = v1 zip v2
-  	val difRes = zipped map { tup: (Double, Double) => tup._1 - tup._2 }
-  	difRes
+	  	require(v1.length == v2.length, "diff - inputs must be of equal length.")
+	  	val zipped = v1 zip v2
+	  	val difRes = zipped map { tup: (Double, Double) => tup._1 - tup._2 }
+	  	difRes
     }
     
     def dotPow(v: List[Double], exp: Double): List[Double] = {
         v map (n => pow(n, exp))
+    }
+    
+    def size(m: List[List[Double]]): (Double, Double) = {
+        val rowCnt = m.length
+        val colCnt = m.head.length
+        (rowCnt, colCnt)
     }
 }
--- src/ngml/regression/SingleFeatureReg.scala
+++ src/ngml/regression/SingleFeatureReg.scala
@@ -4,7 +4,7 @@ import scala.io.Source
 import ngml.matrixlike.MatrixOps._
 import scala.math._
 
-/* Lab: Gradient descent, single variable with bias (1) */
+/* Lab: Gradient descent, single variable with intercept (1) */
 object SingleFeatureReg {
 
     def main(args: Array[String]): Unit = {
@@ -16,48 +16,49 @@ object SingleFeatureReg {
             line.split(",").toList.map(str => str.toDouble))
         println("data: " + data)
         
-        // Intersection prepended to independent variable (city population)
-        val X:List[List[Double]] = data map(_ match {
+        // Intersect prepended to single independent variable (city population)
+        val X = data map(_ match {
 			case x :: xs => 1.0 :: List[Double](x)
 			case _ => throw new NoSuchElementException
         })
-        println("X (input, city population): " + X)
+        println("X is input, i.e. city population (prepended with intersect): " + X)
         
         // target: franchise profit
         val y = data.map(_ drop 1).flatten
-        println("y (target, profit): " + y)
+        println("y is target, i.e. profit: " + y)
         
         // size of data
         val m = y.length
-        println("data size - m: " + m)
+        println("data size m: " + m)
         
-        val theta = List(List(0.0), List(0.0))
+        val theta = List(List(0.0), List(0.0)) // clunky, needs its own Matrix class
         
         val iterations = 1500 // regression cycles
         val alpha = 0.01 // gradient step size
         
         // Initial cost calc
         val J = computeCost(X, y, theta)
-        println("Initial cost calculation - J: " + J)
+        println("Initial cost calculation, i.e. J: " + J)
         
         // theta (slope parameters) calc via gradient descent
-        // and J_history (cost function)
+        // Also J_history of cost function output
         val result:(List[List[Double]], List[Double]) = 
             gradientDescent(X, y, theta, alpha, iterations)
             
         val finalTheta = result._1
-        val J_history = result._2
+        val J_history  = result._2
             
         println("gradient descent - theta final: " + finalTheta.flatten + 
-                "\nJ_history (cost function): " + J_history)
+                "\nJ_history (i.e. cost function): " + J_history)
                 
         // Predict values for population size of 35,000
-        val pop1 = List(List(1.0), List(3.5))
-        val predict1 = matxProd(List(List(1.0, 3.5)), finalTheta)
+        val pop35 = List(List(1.0, 3.5))
+        val predict1 = matxProd(pop35, finalTheta)
         println("Predicted profit for population size of 35,000: " + (predict1(0)(0)) * 10000)
         // Predict values for population size of 70,000
-        val predict2 = matxProd(List(List(1.0, 7.0)), finalTheta)
-        println("Predicted profit for population size of 70,000: " + predict2(0)(0) * 10000)
+        val pop70 = List(List(1.0, 7.0))
+        val predict2 = matxProd(pop70, finalTheta)
+        println("Predicted profit for population size of 70,000: " + (predict2(0)(0)) * 10000)
     }
     
     def computeCost(X: List[List[Double]], y: List[Double], theta: List[List[Double]])
