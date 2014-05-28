c1b4454931ac2870e1e0bc612973d89dc7c18371
GitOutATown <richard@aqume.com>
2014-05-27 17:37:34 -0700

    Adding multiVariant grad descent

---
+++ src/ngml/regression/MultiVariantLinearReg_exer1_main.scala
@@ -0,0 +1,17 @@
+package ngml.regression
+
+import scala.io.Source
+
+object MultiVariantLinearReg_exer1_main {
+
+    def main(args: Array[String]): Unit = {
+        
+        /* Training set:
+         * Predict house price correlated to sq. ft. and number of rooms */ 
+        val file = Source.fromFile("./test_data/ex1data2.txt")
+        val data = file.getLines.toList.map( line => 
+            line.split(",").toList.map(str => str.toDouble))
+        println("data: " + data)
+    }
+
+}
